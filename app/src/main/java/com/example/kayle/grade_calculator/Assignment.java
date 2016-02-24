eec7611f9817823f64e034b5320204a4baac230e
Logan Woodbury <testare@byui.edu>
2016-02-24 14:47:30 -0700

    Added stubs to Assignment and Section

--- app/src/main/java/com/example/kayle/grade_calculator/Assignment.java
+++ app/src/main/java/com/example/kayle/grade_calculator/Assignment.java
@@ -4,4 +4,19 @@ package com.example.kayle.grade_calculator;
  * Created by Testare on 24-Feb-16.
  */
 public class Assignment {
+    float getPointValue() {
+        return 0.0f;
+    }
+
+    void setPointValue() {
+
+    }
+
+    float getPointsEarned() {
+        return 0.0f;
+    }
+
+    void setPointsEarned(float f) {
+
+    }
 }
--- app/src/main/java/com/example/kayle/grade_calculator/Section.java
+++ app/src/main/java/com/example/kayle/grade_calculator/Section.java
@@ -4,4 +4,20 @@ package com.example.kayle.grade_calculator;
  * Created by Testare on 24-Feb-16.
  */
 public class Section {
+
+    public void addAssignment(Assignment a) {
+
+    }
+
+    public void deleteAssignment(Assignment a) {
+
+    }
+
+    public float getWeight () {
+        return 0.0f;
+    }
+
+    public void setWeight (float f) {
+
+    }
 }
