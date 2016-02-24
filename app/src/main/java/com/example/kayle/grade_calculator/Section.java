b7fb3a7e892d55c1146b6f421a3f5c417a8e2c1b
heavymetal899 <heavymetal899@gmail.com>
2016-02-24 15:29:49 -0700

    CalculatorTest first commit

--- app/src/main/java/com/example/kayle/grade_calculator/Section.java
+++ app/src/main/java/com/example/kayle/grade_calculator/Section.java
@@ -1,12 +1,15 @@
 package com.example.kayle.grade_calculator;
 
+import java.util.ArrayList;
+
 /**
  * Created by Testare on 24-Feb-16.
  */
 public class Section {
+    ArrayList<Assignment> assignments = new ArrayList<>();
 
     public void addAssignment(Assignment a) {
-
+        assignments.add(a);
     }
 
     public void deleteAssignment(Assignment a) {
