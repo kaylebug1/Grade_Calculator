92346f48cc0614456f09b1ce76eb8e5118baa727
Logan Woodbury <testare@byui.edu>
2016-02-24 08:35:32 -0700

    Settings stub, Settings test class

--- app/src/main/java/com/example/kayle/grade_calculator/Settings.java
+++ app/src/main/java/com/example/kayle/grade_calculator/Settings.java
@@ -4,4 +4,35 @@ package com.example.kayle.grade_calculator;
  * Created by Testare on 24-Feb-16.
  */
 public class Settings {
+    public final static float UNSET = -1.0f;
+    private float baseGrade;
+    private float targetGrade;
+
+    Settings(float grade,boolean isBaseGrade) {
+        if(isBaseGrade) {
+            setBaseGrade(grade);
+        }
+        else {
+            setTargetGrade(grade);
+        }
+    }
+    Settings (float baseGrade) {
+        setBaseGrade(baseGrade);
+    }
+    Settings () {
+        setBaseGrade(UNSET);
+    }
+
+    public float getBaseGrade() {
+        return 0.0f;
+    }
+    public void setBaseGrade(float grade) {
+
+    }
+    public float getTargetGrade() {
+        return 0.0f;
+    }
+    public void setTargetGrade(float grade) {
+
+    }
 }
---
+++ app/src/test/java/com/example/kayle/grade_calculator/SettingsTest.java
@@ -0,0 +1,38 @@
+package com.example.kayle.grade_calculator;
+
+import org.junit.Test;
+
+import dalvik.annotation.TestTargetClass;
+
+/**
+ * Created by Testare on 24-Feb-16.
+ */
+public class SettingsTest {
+    Settings s = new Settings();
+    @Test
+    public void testGradeSetters() {
+
+        s = new Settings(1.0f);
+        assert (s.getBaseGrade() == 1.0f);
+        assert (s.getTargetGrade() == Settings.UNSET);
+        s = new Settings(2.0f, false);
+        assert (s.getBaseGrade() == Settings.UNSET);
+        assert (s.getTargetGrade() == 2.0f);
+        s = new Settings(3.0f, true);
+        assert (s.getBaseGrade() == 3.0f);
+        assert (s.getTargetGrade() == Settings.UNSET);
+        s = new Settings();
+        assert (s.getBaseGrade() == Settings.UNSET);
+        assert (s.getTargetGrade() == Settings.UNSET);
+        s.setBaseGrade(5.0f);
+        assert (s.getBaseGrade() == 5.0f);
+        assert (s.getTargetGrade() == Settings.UNSET);
+        s.setTargetGrade(6.0f);
+        assert (s.getTargetGrade() == 6.0f);
+        assert (s.getBaseGrade() == Settings.UNSET);
+        s.setBaseGrade(7.0f);
+        assert (s.getBaseGrade() == 7.0f);
+        assert (s.getTargetGrade() == Settings.UNSET);
+
+    }
+}
