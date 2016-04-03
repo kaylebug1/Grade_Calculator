0541a57f83187dc2820a7bfd3782ddb4c60e2fd7
Kayle Sillito <kaylebug1@yahoo.com>
2016-04-02 19:37:04 -0600

    Layout looks better and actionbar not shown

--- app/src/main/java/com/example/kayle/grade_calculator/MainActivity.java
+++ app/src/main/java/com/example/kayle/grade_calculator/MainActivity.java
@@ -5,25 +5,18 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.InputType;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,9 +32,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         context = this;
+        //ActionBar actionBar = getActionBar();
+        //actionBar.hide();
 
         Course.loadCourselist(this);
         List<Course> courseList = Course.getCourseList();
